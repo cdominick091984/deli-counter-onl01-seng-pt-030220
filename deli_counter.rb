702aa8f31b2ffe026d280702f30d433ab172d320
Domenico Cascone <dcascone84@gmail.com>
2020-03-11 08:33:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+require "pry"
+
+def line(katz_deli)
+  line_position = []
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  elsif katz_deli.length > 0
+    katz_deli.each.with_index(1) do |name, number|
+      line_position.push("#{number}. #{name}")
+    end
+    puts "The line is currently: #{line_position.join(" ")}"
+  end
+end
+
+def take_a_number(katz_deli, name)
+  if katz_deli.length == 0
+      katz_deli.push(name)
+  end
+puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
